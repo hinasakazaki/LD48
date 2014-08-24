f135726c42a66da8af2cf45ec77eb4f4cfcd0fd9
Hina Sakazaki <hinasakazaki@gmail.com>
2014-08-23 22:10:53 -0700

    Getting done with level 2

---
+++ Assets/ParallexScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParallexScript : MonoBehaviour {
+
+	public Transform[] backgrounds;
+	private float[] parallexScaling;
+	public float smoothing = 1f;
+
+	private Transform cam; //main camera transform
+	private Vector3 previousCamPos; //position of camera in previous frame
+
+	void Awake(){
+		//set up references
+		cam = Camera.main.transform;
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		previousCamPos = cam.position;
+		parallexScaling = new float[backgrounds.Length];
+
+		for (int i = 0; i < backgrounds.Length; i++) {
+			parallexScaling[i] = backgrounds[i].position.z*-1;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		for (int i = 0; i < backgrounds.Length; i++) {
+			float parallex = (previousCamPos.x - cam.position.x)*parallexScaling[i];
+			//set target x position which is current position + Parallex 
+			float backgroundTargetPosX = backgrounds[i].position.x + parallex;
+
+			Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+			//fade between current position and target position using lerp
+			backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing*Time.deltaTime);
+		}
+		previousCamPos = cam.position;
+	}
+}
---
+++ Assets/TalkScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TalkScript : MonoBehaviour {
+	public GameObject talk;
+	bool collided = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		talk.SetActive (collided);
+
+	}
+
+	void OnCollisionEnter2D(Collision2D coll) {
+		collided = true;
+		Invoke ("Clear", 3f);
+	}
+
+	void Clear() {
+		collided = false;
+		talk.SetActive (false);
+	}
+}
