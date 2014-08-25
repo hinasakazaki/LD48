ad345b43cecd69b27305160297886c1c19a026f8
Hina Sakazaki <hinasakazaki@gmail.com>
2014-08-24 17:52:47 -0700

    line draw works :D

---
+++ Assets/DrawLineScript.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class DrawLineScript : MonoBehaviour {
+	public PlayerScript hero;
+	private LineRenderer line;
+	private bool stitching;
+	private bool wasStitching;
+	private List<Vector3> pointsList;
+	private Vector3 heroPos;
+
+	struct myLine {
+		public Vector3 StartPoint;
+		public Vector3 EndPoint;
+	};
+
+	void Awake() {
+		line = gameObject.AddComponent<LineRenderer> ();
+		line.material = new Material (Shader.Find ("Particles/Additive"));
+		line.SetVertexCount (0);
+		line.SetWidth (0.3f, 0.3f);
+		line.SetColors (Color.white, Color.white);
+		line.useWorldSpace = true;
+		stitching = false;
+		pointsList = new List<Vector3>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (hero.isStitching()) {
+			stitching = true;
+		}
+		else if (hero.isStitching() == false){
+			pointsList = new List<Vector3>();
+			stitching = false;
+		}
+		//Drawing line
+		if (stitching) {
+			heroPos = hero.transform.position;
+			heroPos.z = 0;
+			if (!pointsList.Contains(heroPos)) {
+				pointsList.Add(heroPos);
+				line.SetVertexCount (pointsList.Count);
+				line.SetPosition (pointsList.Count -1, (Vector3)pointsList[pointsList.Count -1]);
+			}
+		}
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour {
+	private bool stitching = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+
+	public bool isStitching() {
+		return stitching;
+	}
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("stitch")) {
+						stitching = true;
+				} else if (Input.GetButtonUp ("stitch")) {
+						stitching = false;
+				}
+
+	}
+}
